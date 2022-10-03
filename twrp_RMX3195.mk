5c2d4bef4a438a085d03a3cc0f769aa450c87df4
vsekretov <93854519+vsekretov@users.noreply.github.com>
2022-10-03 12:31:06 +0700

    Update twrp_RMX3195.mk

--- twrp_RMX3195.mk
+++ twrp_RMX3195.mk
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-LOCAL_PATH := device/realme/KG7n
+LOCAL_PATH := device/tecno/KG7n
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
