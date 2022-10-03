21f18a2bdde5871b2fc32421475783caf5b4d1a0
vsekretov <93854519+vsekretov@users.noreply.github.com>
2022-10-03 13:43:04 +0700

    Add files via upload

--- twrp_KG7n.mk
+++ twrp_KG7n.mk
@@ -19,7 +19,7 @@ LOCAL_PATH := device/tecno/KG7n
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
