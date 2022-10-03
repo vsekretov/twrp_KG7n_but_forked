2434aba715b5dc75c5cafffd4fc78273f663df7a
vsekretov <93854519+vsekretov@users.noreply.github.com>
2022-10-03 14:01:02 +0700

    Update twrp_KG7n.mk

--- twrp_KG7n.mk
+++ twrp_KG7n.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+#$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := KG7n
 PRODUCT_NAME := twrp_KG7n
